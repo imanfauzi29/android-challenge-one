e04296fed8e0491c7346694d7e2af4556f74b274
M Rohmatullah <m.rohmatullah10@gmail.com>
2020-08-05 18:29:37 +0700

    done

--- app/src/main/java/com/mycorp/challengeone/three/ThreeActivity.java
+++ app/src/main/java/com/mycorp/challengeone/three/ThreeActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.mycorp.challengeone.MainActivity;
 import com.mycorp.challengeone.R;
 
 import java.util.regex.Pattern;
@@ -19,37 +20,44 @@ public class ThreeActivity extends AppCompatActivity {
 
     EditText email, pass;
     Button submit;
-    String emails, password;
+    String emails, passwords;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
 
-        email = findViewById(R.id.emailss);
+        email = findViewById(R.id.email);
         pass = findViewById(R.id.password);
         submit = findViewById(R.id.btn_submit);
 
-        System.out.println(email.getText().toString());
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println(password);
-                System.out.println("Ini password: "+emails);
-                if (password == null) {
+                emails = email.getText().toString();
+                passwords = pass.getText().toString();
+
+                if (passwords.equalsIgnoreCase("") || emails.equalsIgnoreCase("")) {
                     Toast.makeText(getApplicationContext(), "Field tidak boleh kosong!", Toast.LENGTH_LONG).show();
-                }else if (isValidateEmail(emails)) {
-                    System.out.println("ok");
+                } else if(passwords.length() < 8) {
+                    Toast.makeText(getApplicationContext(), "Password  kurang dari 8", Toast.LENGTH_LONG).show();
+                }else if (!isValidateEmail(emails)) {
+                    Toast.makeText(getApplicationContext(), "Email tidak valid", Toast.LENGTH_LONG).show();
+                }else if (!isValidatePassword(passwords)) {
+                    Toast.makeText(getApplicationContext(), "Password bukan alphanumeric", Toast.LENGTH_LONG).show();
+                }else {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
                 }
             }
         });
     }
-
-    public static boolean isValidateEmail(CharSequence email) {
-        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    public static boolean isValidateEmail(String email) {
+        return (Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 
     public static boolean isValidatePassword(String pass) {
-        return (!TextUtils.isEmpty(pass) && pass.matches("^[a-zA-Z0-9]$"));
+        return (pass.matches("^[a-zA-Z0-9]*$"));
     }
 }
